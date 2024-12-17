7d44d2732451924e74fd36cbeb7b9219f7c61c99
jaelsonsantos1 <jaelsons860@gmail.com>
2024-12-17 20:13:55 -0300

    feat: add seed file

---
+++ src/app/home/page.tsx
@@ -0,0 +1,19 @@
+
+
+export default function HomePage() {
+    return (
+        <div
+            className="flex flex-col items-center justify-center h-screen"
+        >
+            <div
+                className="w-2 h-2 bg-black"
+            >
+                <h1
+                    className="text-2xl"
+                >
+                    Hello World
+                </h1>
+            </div>
+        </div>
+    )
+}
